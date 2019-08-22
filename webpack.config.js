50d421bd551bc18569ce5abd8118320c076c5289
Cesare Ghirelli <cghirelli@arpae.it>
2019-08-22 10:52:44 +0200

    add mocha , expect, etc. Main test passes

--- webpack.config.js
+++ webpack.config.js
@@ -22,16 +22,19 @@ module.exports = {
     // Loaders
     module: {
         rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: ['babel-loader'],
-            },
-            // CSS Files
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+            { test: /\.jsx?$/, exclude: /(node_modules)/, use: ['babel-loader'] },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+            { test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader'},
+            { test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/, use: 'file-loader' },
+            { test: /\.(png|jp(e*)g|svg)$/, use: [{loader: 'url-loader', options:
+                {
+                limit: 8000, // Convert images < 8kb to base64 strings
+                name: 'images/[hash]-[name].[ext]'
+                }
             }
+        ]
+      }
         ],
      },
      stats: {

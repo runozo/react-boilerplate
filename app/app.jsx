9756f60e616253e7d524d5f202223ea755d7467d
Cesare Ghirelli <cghirelli@arpae.it>
2019-10-01 10:01:57 +0200

    moved to ES6 style import

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
-// Load foundation
-// $(document).foundation();
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 
 // App css
 require('applicationStyles')
